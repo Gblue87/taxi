c49e56959834c675df0d59d418dc8e7c3818dbf9
Georgi <georgi@stenik.bg>
2016-12-06 13:55:29 +0200

    logic for form in hotels view

--- src/NewVision/ServicesBundle/Controller/ServicesFrontendController.php
+++ src/NewVision/ServicesBundle/Controller/ServicesFrontendController.php
@@ -67,6 +67,65 @@ class ServicesFrontendController extends Controller
             $to = true;
         }
 
+        $form = $this->container->get('form.factory')->create('order', new Order(), array(
+            'method' => 'POST',
+            'action' => $this->generateUrl('airport_view', array('slug' => $airport->getSlug()))
+        ));
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            $session = $this->get('session');
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $data->setNo(rand(5, 16).time());
+                $data->setType('hotel');
+                $em->persist($data);
+                $em->flush();
+
+                $price = $data->getAmount() * $settingsManager->get('surcharge', 1);
+                if(!$price){
+                    throw $this->createNotFoundException();
+                }
+
+                //LIVE "https://www.paypal.com/cgi-bin/webscr",
+                $paypalForm = array(
+                    'action' => "https://www.sandbox.paypal.com/cgi-bin/webscr",
+                    'fields' => array(
+                        'cmd' => "_ext-enter",
+                        'redirect_cmd' => "_xclick",
+                        'business' => 'taxichester.uk@gmail.com',
+                        'invoice' => $data->getNo(),
+                        'amount' => $price,
+                        'currency_code' => 'GBP',
+                        'paymentaction' => "sale",
+                        'return' => $request->getSchemeAndHttpHost().$this->generateUrl('paypal_success', array('id' => $data->getNo())),
+                        'cancel_return' => $request->getSchemeAndHttpHost().$this->generateUrl('paypal_success', array('id' => $data->getNo())),
+                        'notify_url' => $request->getSchemeAndHttpHost().$this->generateUrl('paypal_notify', array('id' => $data->getNo())),
+                        'item_name' => "TaxiChester Order #".$data->getNo(),
+                        'lc' => "en_GB",
+                        'charset' => "utf-8",
+                        'no_shipping' => "1",
+                        'no_note' => "1",
+                        'image_url' => "",
+                        'email' => $data->getEmail(),
+                        'first_name' => $data->getName(),
+                        'last_name' => $data->getFamily(),
+                        'custom' => $data->getNo(),
+                        'cs' => "0",
+                        'page_style' => "PayPal"
+                    )
+                );
+                return $this->redirectToRoute('paypal_gateway', array('form' => $paypalForm));
+
+            } else {
+                $session->getFlashBag()->clear();
+                $session->getFlashBag()->add(
+                    'error',
+                    'applyment_error'
+                );
+            }
+        }
+
         $this->generateSeoAndOgTags($hotel);
 
         $offer['id'] = $hotel->getId();
@@ -76,6 +135,7 @@ class ServicesFrontendController extends Controller
             'to' => $to,
             'offerPoint' => $point,
             'offer' => json_encode($offer),
+            'form' => $form->createView(),
         );
     }
 }
