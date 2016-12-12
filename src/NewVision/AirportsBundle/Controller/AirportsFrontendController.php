b65397761e2ef7f60f2e7e2a6b696e9bf675a9e6
Georgi <georgi@stenik.bg>
2016-12-12 20:02:15 +0200

    worldpay started

--- src/NewVision/AirportsBundle/Controller/AirportsFrontendController.php
+++ src/NewVision/AirportsBundle/Controller/AirportsFrontendController.php
@@ -85,6 +85,7 @@ class AirportsFrontendController extends Controller
                 $data->setNo(rand(5, 16).time());
                 $data->setType('airport');
                 $data->setPaymentType($data->getPaymentType());
+                $data->setPaymentStatus('new');
                 $em->persist($data);
                 $em->flush();
 
@@ -124,7 +125,43 @@ class AirportsFrontendController extends Controller
                     $this->get('session')->set('paypalForm', $paypalForm);
                     return $this->redirectToRoute('paypal_gateway');
                 }elseif($data->getPaymentType() != null && $data->getPaymentType() == 'worldpay'){
+                    if (empty($data) || ($data->getPaymentStatus() != "new"))
+                        throw $this->createNotFoundException();
+                    define('WPAY_TEST_MODE',            true);
+                    define('WPAY_INSTALLATION_ID',      1110266);
+                    define('WPAY_ACCOUNT_ID',           "CHESTERTRAV1M2");
+                    define('WPAY_CURRENCY',             "GBP");
+                    define('WPAY_MD5_SECRET',           "1Qq!;lgdl;gioijgiojio");
+                    define('WPAY_RESPONSE_PASSWORD',    "321W%4fdg5fg/fgfgg");
+                    define('WPAY_CART_ID_PREFIX',       "");
+                    define('WPAY_INVOICE_ID_ADD',       0);
+                    $testMode = WPAY_TEST_MODE ? "100" : "0";
+                    $signature = md5(WPAY_MD5_SECRET . ":" . WPAY_CURRENCY . ":$price:$testMode:" . WPAY_INSTALLATION_ID);
 
+                    $form = array(
+
+                        'action' => WPAY_TEST_MODE
+                            ? "https://secure-test.worldpay.com/wcc/purchase"
+                            : "https://secure.worldpay.com/wcc/purchase",
+
+                        'fields' => array(
+                            'instId' => WPAY_INSTALLATION_ID,
+                            'amount' => $price,
+                            'cartId' => WPAY_CART_ID_PREFIX . ($data->getId() + WPAY_INVOICE_ID_ADD),
+                            'currency' => WPAY_CURRENCY,
+                            'testMode' => $testMode,
+                            'desc' => "TaxiChester Order #$data->getId()",
+                            'authMode' => "A",
+                            'accId1' => WPAY_ACCOUNT_ID,
+                            'withDelivery' => "false",
+                            'fixContact' => "false",
+                            'hideContact' => "false",
+                            'signature' => $signature
+                        )
+                    );
+
+                    $this->get('session')->set('worldpayForm', $paypalForm);
+                    return $this->redirectToRoute('worldpay_gateway');
                 }elseif($data->getPaymentType() != null && $data->getPaymentType() == 'cash'){
                     $data->setPaymentStatus('cash-order');
                     $em->persist($data);
@@ -162,6 +199,17 @@ class AirportsFrontendController extends Controller
      */
     public function paypalGatewayAction(Request $request){
         $form = $this->get('session')->get('paypalForm');
+        return array(
+            'form' => $form,
+        );
+    }
+
+    /**
+     * @Route("/worldpay-gateway", name="worldpay_gateway")
+     * @Template("NewVisionAirportsBundle:Frontend:gateway.html.twig")
+     */
+    public function worldpayGatewayAction(Request $request){
+        $form = $this->get('session')->get('worldpayForm');
         return array(
             'form' => $form,
         );
@@ -215,6 +263,32 @@ class AirportsFrontendController extends Controller
     }
 
     /**
+     * @Route("/worldpay-success/{id}", name="worldpay_success")
+     * @Template("NewVisionAirportsBundle:Frontend:paypalSuccess.html.twig")
+     */
+    public function worldpaySuccessAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ordersRepository = $em->getRepository('NewVisionFrontendBundle:Order');
+        if (!preg_match('/^\d+$/', $id))
+            throw $this->createNotFoundException();
+        $order = $ordersRepository->findOneByNo($id);
+        if (empty($order))
+            throw $this->createNotFoundException();
+
+        if ($order->getPaymentStatus() != 'paid') {
+            return $this->redirectToRoute('paypal_error');
+        }
+
+
+        // $this->sendOrderAdminMail($order);
+        // $this->sendOrderUserMail($order);
+        return array(
+            'order' => $order,
+        );
+    }
+
+    /**
      * @Route("/paypal-error", name="paypal_error")
      * @Template("NewVisionAirportsBundle:Frontend:paypalError.html.twig")
      */
@@ -222,6 +296,87 @@ class AirportsFrontendController extends Controller
     {
         return array(
         );
+    }
+
+    /**
+     * @Route("/worldpay-notify", name="worldpay_notify")
+     */
+    public function worldpayNotifyAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $settingsManager = $this->get('newvision.settings_manager');
+        $ip = $this->container->get('request')->getClientIp();
+        file_put_contents('/home/simplec/taxi/web/test.txt',$ip, FILE_APPEND);
+        if (empty($ip) ||
+            (substr(gethostbyaddr($ip), -13) != ".worldpay.com")
+        )
+            throw $this->createNotFoundException();
+
+        $p = $request->request->all();
+        file_put_contents('/home/simplec/taxi/web/test.txt',print_r($p, true), FILE_APPEND);
+        $checks = explode(' ', "instId callbackPW AVS cartId currency amount transId transStatus");
+
+        foreach ($checks as $key)
+            if (!isset($p[$key]))
+                throw $this->createNotFoundException();
+
+
+        if (
+            $p['instId'] != WPAY_INSTALLATION_ID ||
+            $p['callbackPW'] != WPAY_RESPONSE_PASSWORD ||
+            $p['currency'] != WPAY_CURRENCY ||
+            (substr($p['AVS'], 0, 1) != "2" && !WPAY_TEST_MODE || substr($p['AVS'], 0, 1) != "1") || !preg_match('/^\d+$/', $p['transId']))
+        {
+            throw $this->createNotFoundException();
+        }
+// GET ORDER
+
+        $id = substr($p['cartId'], strlen(WPAY_CART_ID_PREFIX));
+        file_put_contents('/home/simplec/taxi/web/test.txt', 'ID:'. $id, FILE_APPEND);
+        if (!preg_match('/^\d+$/', $id))
+            throw $this->createNotFoundException();
+        $id -= WPAY_INVOICE_ID_ADD;
+        file_put_contents('/home/simplec/taxi/web/test.txt', 'ID:'. $id, FILE_APPEND);
+
+        $order = $em->getRepository('NewVisionFrontendBundle:Order')->findOneByNo($id);
+
+        file_put_contents('/home/simplec/taxi/web/test.txt', 'ID:'. $order->getNo(), FILE_APPEND);
+        if (empty($order) || ($order->getPaymentStatus() != "new"))
+            throw $this->createNotFoundException();
+
+
+// TRANSACTION - FAILED
+
+        file_put_contents('/home/simplec/taxi/web/test.txt', $p['transStatus'], FILE_APPEND);
+        if (($p['transStatus'] != 'Y'))
+            $order->setPaymentStatus('payment-failed');
+
+// TRANSACTION - SUCCESS
+
+        else {
+
+            // AMOUNT MISSMATCH
+            $price = $order->getAmount()*$settingsManager->get('surcharge');
+            if (($price === false) ||
+                ((int) $price > (int) $p['amount'])
+            )
+                $status = "payment-failed";
+
+            // AMOUNT OK
+            else
+                $status = "paid";
+
+        file_put_contents('/home/simplec/taxi/web/test.txt', $status, FILE_APPEND);
+           $order->setPaymentStatus($status);
+           $em->persist($order);
+           $em->flush();
+            // SEND MAILS IF OK
+            if ($status == "paid") {
+                $this->sendOrderAdminMail($order);
+                $this->sendOrderUserMail($order);
+                return $this->redirectToRoute('worldpay_success', array('id' => $order->getId()));
+            }
+        }
     }
 
     /**
